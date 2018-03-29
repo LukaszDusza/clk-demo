66d46b424d3f80f4503a28dffc98231af001289a
lukaszdusza <lukaszdusza280@gmail.com>
2018-03-29 16:24:12 +0200

    pierwszy commit

---
+++ src/test/java/com/clk/clkdemo/ClkDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clk.clkdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClkDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
