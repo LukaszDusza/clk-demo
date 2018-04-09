14038fa8f8f2b3eebc0af52763f0f5f423685feb
MichalSkiba <michalskiba.ms@gmail.com>
2018-04-09 12:22:07 +0200

    Tabelki i relacje

---
+++ src/main/java/com/clk/clkdemo/ClkDemoApplication.java
@@ -0,0 +1,12 @@
+package com.clk.clkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClkDemoApplication.class, args);
+    }
+}
