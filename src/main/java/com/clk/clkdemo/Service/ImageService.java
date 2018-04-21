382b46c0c9503280de6c9394246dbd1129751bc2
MichalSkiba <michalskiba.ms@gmail.com>
2018-04-21 10:51:01 +0200

    Service Implement

--- src/main/java/com/clk/clkdemo/Service/ImageService.java
+++ src/main/java/com/clk/clkdemo/Service/ImageService.java
@@ -1,4 +1,8 @@
 package com.clk.clkdemo.Service;
 
+import com.clk.clkdemo.model.entitis.Image;
+
 public interface ImageService {
+
+    public void saveImage (Image image);
 }
--- src/main/java/com/clk/clkdemo/Service/MinutiaService.java
+++ src/main/java/com/clk/clkdemo/Service/MinutiaService.java
@@ -1,4 +1,8 @@
 package com.clk.clkdemo.Service;
 
+import com.clk.clkdemo.model.entitis.Minutia;
+
 public interface MinutiaService {
+
+    public void saveMinutia(Minutia minutia);
 }
