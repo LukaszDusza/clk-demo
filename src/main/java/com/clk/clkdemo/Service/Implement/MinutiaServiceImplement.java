c1b95b578bcfd9ef14f03e248d3df7004ec203a9
MichalSkiba <michalskiba.ms@gmail.com>
2018-04-23 09:26:54 +0200

    Implementacja

--- src/main/java/com/clk/clkdemo/Service/Implement/MinutiaServiceImplement.java
+++ src/main/java/com/clk/clkdemo/Service/Implement/MinutiaServiceImplement.java
@@ -1,11 +1,19 @@
 package com.clk.clkdemo.Service.Implement;
 
+import com.clk.clkdemo.DAO.MinutiaRepository;
 import com.clk.clkdemo.Service.MinutiaService;
 import com.clk.clkdemo.model.entitis.Minutia;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MinutiaServiceImplement implements MinutiaService {
+
+    @Autowired
+    MinutiaRepository minutiaRepository
+
     @Override
     public void saveMinutia(Minutia minutia) {
+        minutiaRepository.save(minutia);
+
 
     }
 }
